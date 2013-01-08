286554178c7aa4eebbb276e273cb7251f34daf25
Brian <brian@ubuntu.(none)>
2013-01-07 18:21:49 -0800

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    $("ul>li>p:not(#navigation_header)").hide();
+    
+});
+
+$(document).ready(function() {
+    $("#left_panel").one("click", function ()  {
+        $("#left_panel").animate ({
+            width:"30%"
+        },1500);
+        $("ul>li>p:not(#navigation_header)").delay(1500).each(function(index){
+            $(this).delay(500*index).fadeIn(300);
+        });
+    });
+});
+
+$(document).ready(function() {
+    $("#navigation_header").click(function() {
+        $('html,body').animate({scrollTop: 0}, 500);
+    });
+});
+
+$(document).ready(function() {
+    $('#navigation_container p').hover(function() {
+        if(!$(this).hasClass('clicked')) {
+            $(this).dequeue().stop().animate({color:'#000'});
+        }
+    }, function() {
+        if(!$(this).hasClass('clicked')) { 
+            $(this).addClass('clicked').animate({color:'#FFF'}, 'normal', 'linear', function() {
+                $(this).removeClass('clicked').dequeue();
+            });
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#navigation_container p').click(function() {
+        var clicked = $(this);
+        var clickClass = $(this).attr("class");
+        $('html,body').animate ({
+            scrollTop: $(".right_panel#"+clickClass).offset().top},
+        500);
+        $('#navigation_container p').stop().animate({color:"#FFF"}).removeClass('clicked');
+        $(this).stop().animate({color:'#000'}).addClass('clicked');
+        $('#left_panel').css("border-right", "5px solid white");
+    });
+});
+
+$(function () {
+    $('.antiscroll-wrap').antiscroll();
+});
